363b16d3c557a9cd11211c82722078fc4a8bf532
kostya4118 <kostya4118@gmail.com>
2022-08-15 18:06:48 +0500

    class Point

--- Program.cs
+++ Program.cs
@@ -6,26 +6,38 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sim1 = '*';
+            //int x1 = 1;
+            //int y1 = 3;
+            //char sim1 = '*';
 
-            Draw(x1, y1, sim1);
+            //Draw(x1, y1, sim1);
 
-            int x2 = 4;
-            int y2 = 5;
-            char sim2 = '#';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sim = '*';
+            p1.Draw();
 
-            Draw(x2, y2, sim2);
+            //int x2 = 4;
+            //int y2 = 5;
+            //char sim2 = '#';
+
+            //Draw(x2, y2, sim2);
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sim = '#';
+            p2.Draw();
 
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char sim)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sim);
-        }
+        //static void Draw(int x, int y, char sim)
+        //{
+        //    Console.SetCursorPosition(x, y);
+        //    Console.Write(sim);
+        //}
     }
 }
 
