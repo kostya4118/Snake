3a406be3d4f240c0d361258162f6a935e24b099f
kostya4118 <kostya4118@gmail.com>
2022-08-17 12:45:07 +0500

    Encapsulation

--- Point.cs
+++ Point.cs
@@ -7,6 +7,18 @@ namespace Snake
         public int y;
         public char sim;
 
+        public Point()
+        {
+            //Console.WriteLine("Новая точка");
+        }
+
+        public Point(int _x, int _y, char _sim)
+        {
+            x = _x;
+            y = _y;
+            sim = _sim;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
