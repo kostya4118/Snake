294fff0a003806cfc3af83d8e1e667b09a68de47
kostya4118 <kostya4118@gmail.com>
2022-08-17 13:22:57 +0500

    RAM

--- Program.cs
+++ Program.cs
@@ -4,20 +4,17 @@ namespace Snake
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        //static void Main(string[] args)
+        //{
 
-            Point p1 = new Point(1, 3, '*');
-            //p1.x = 1;
-            //p1.y = 3;
-            //p1.sim = '*';
-            p1.Draw();
+        //    Point p1 = new Point(1, 3, '*');
+        //    p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+        //    Point p2 = new Point(4, 5, '#');
+        //    p2.Draw();
 
-            Console.ReadLine();
-        }
+        //    Console.ReadLine();
+        //}
 
     }
 }
